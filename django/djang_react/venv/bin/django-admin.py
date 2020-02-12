260d3ecb51b2b8c6c745d56f385e2e31337589b8
salamentic <anrao3@buffalo.edu>
2020-02-12 17:34:44 -0500

    Created a django model

---
+++ django/djang_react/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/anrao3/cse442-semester-project-tapp/django/djang_react/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

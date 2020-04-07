93410cf6db06691beab95ae5e76e4448cf28ff82
Aniruddh Rao <anrao3@buffalo.edu>
2020-04-06 23:15:07 -0400

    Implemeted instructors endpoint on the backend #55

--- django/django_react/accounts/urls.py
+++ django/django_react/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .api import RegAPI, LogAPI, UserAPI
+from .api import RegAPI, LogAPI, UserAPI, InstructAPI
 from knox import views as knox_views
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
   path('api/auth/register', RegAPI.as_view()),
   path('api/auth/login', LogAPI.as_view()),
   path('api/auth/user', UserAPI.as_view()),
+  path('api/instructors', InstructAPI.as_view()),
   path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
 ]

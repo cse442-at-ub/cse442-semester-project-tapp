030139216a9e49fae7a47940e6e2d12ef45eb94e
Aniruddh Rao <anrao3@buffalo.edu>
2020-05-03 00:45:52 -0400

    Implemented frontend for switching classes #61

--- django/django_react/tapp-react/src/topbar.js
+++ django/django_react/tapp-react/src/topbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link, Redirect } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 import Button from 'react-bootstrap/Button';
 
@@ -30,12 +32,21 @@ export class TopBar extends Component{
     )
   }
   if (isAuthenticated){
+    const userlist = JSON.parse(this.props.myuser.listcourses);
     return (
     <Navbar style={{backgroundColor: '#36453b'}} variant="dark" expand="lg" fixed="top" fluid>
       <Navbar.Brand>TApp</Navbar.Brand>
       <Navbar.Toggle aria-controls="response-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="ml-auto">
+      <DropdownButton variant="info" className="mx-2 ml-md-0" title={this.props.myuser.course}>
+	    {
+              Object.keys(userlist).map((id,instr) => (
+		(this.props.myuser.course===id) ?
+	        null :<Dropdown.Item > {id} </Dropdown.Item> 
+	      ))
+	    }
+      </DropdownButton>
       <Button variant="info" className="mx-2 ml-md-0" onClick={this.props.prof}> Profile </Button>
       <Link to="/">
           <Button variant="info" className="mx-2 ml-md-0" onClick={this.props.logout}> Logout </Button>

4813bd0300fa35a664c586a24de0c54c746a827e
hussham <husshamdan97@gmail.com>
2020-05-04 04:54:50 -0400

    Implemented backend delete http request for queue #66

--- django/django_react/virtual_queue/urls.py
+++ django/django_react/virtual_queue/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .api import OHQueueViewSet, QueueUpdateView, OHQueueViewPost
+from .api import OHQueueViewSet, QueueUpdateView, OHQueueViewPost, QueueDeleteView
 from rest_framework import routers
 from django.conf.urls import url
 
 urlpatterns = [
   path('api/officeHours/Get', OHQueueViewSet.as_view()),
   path('api/officeHours/Post', OHQueueViewPost.as_view()),
-  path('api/officeHours/Update/<int:pk>/', QueueUpdateView.as_view())
+  path('api/officeHours/Update/<int:pk>/', QueueUpdateView.as_view()),
+  path('api/officeHours/Delete/<int:pk>/', QueueDeleteView.as_view())
 ]

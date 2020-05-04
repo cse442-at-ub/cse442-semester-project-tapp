6206d73e940ea6141d9d8e3f9e4975be459a0805
hussham <husshamdan97@gmail.com>
2020-05-03 23:53:42 -0400

    Added endpoint to make patch requests #60

--- django/django_react/virtual_queue/urls.py
+++ django/django_react/virtual_queue/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .api import OHQueueViewSet
+from .api import OHQueueViewSet, QueueUpdateView, OHQueueViewPost
 from rest_framework import routers
+from django.conf.urls import url
 
-router = routers.DefaultRouter()
-router.register('api/officeHours', OHQueueViewSet, basename="officeHours")
-urlpatterns = router.urls
+urlpatterns = [
+  path('api/officeHours/Get', OHQueueViewSet.as_view()),
+  path('api/officeHours/Post', OHQueueViewPost.as_view()),
+  path('api/officeHours/Update/<int:pk>/', QueueUpdateView.as_view())
+]
